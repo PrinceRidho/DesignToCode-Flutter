5e0367a31b96c5fe0fdcf37b9075eebd743e46a3
Ridho Weedy <71997641+PrinceRidho@users.noreply.github.com>
2022-07-30 23:39:26 +0700

    Add files via upload
    This is splash page version 2 of mine

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,77 +1,60 @@
-import 'package:bwa_cozy/pages/home_page.dart';
-import 'package:bwa_cozy/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:nyari_1/theme.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        bottom: false,
-        child: Stack(
-          children: [
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Image.asset('assets/splash_image.png')),
-            Padding(
-              padding: const EdgeInsets.only(top: 50, left: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/logo.png'),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: edge),
+          child: Column(
+            children: [
+              SizedBox(height: 96),
+              Center(
+                child: Text(
+                  'Nusantara',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    shadows: <Shadow>[
+                      Shadow(
+                        offset: Offset(0.0, 5.0),
+                        blurRadius: 4.0,
+                        color: Color.fromARGB(255, 0, 0, 0),
                       ),
-                    ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Find Cozy House\nto Stay and Happy',
-                    style: blackTextStyle.copyWith(fontSize: 24),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
-                    style: greyTextStyle.copyWith(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    width: 210,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => HomePage()),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: purpleColor,
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(17),
-                        ),
-                      ),
-                      child: Text(
-                        'Explore Now',
-                        style: whiteTextStyle,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 17),
+              Image.asset(
+                'assets/img_splash1.png',
+                width: 331,
+              ),
+              SizedBox(height: 1),
+              Image.asset('assets/img_splash2.png', width: 144, height: 77),
+              SizedBox(height: 27),
+              Container(
+                width: MediaQuery.of(context).size.width - (2 * edge),
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: whiteColor,
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(17),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore Now',
+                    style: purpleTextStyle.copyWith(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
